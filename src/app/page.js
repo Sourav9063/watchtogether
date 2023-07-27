24967814b9000590601e824dfae21ca2c519410e
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2023-07-28 03:22:35 +0600

    react plaayer used

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import VideoPlayer from "@/components/videoPlayer/VideoPlayer";
-import CustomLink from "@/components/customButtons/CustomLink";
+import ReactVideoPlayer from "@/components/reactPlayer/ReactPlayer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <CustomLink />
-      <VideoPlayer />
+      <ReactVideoPlayer />
     </main>
   );
 }
