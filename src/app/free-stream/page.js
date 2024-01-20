df7c663c39e69c99a3712dd2e9477871c0a0ef41
Sourav <sourav.ahmed5654@gmail.com>
2024-01-21 02:45:12 +0600

    links added

--- src/app/free-stream/page.js
+++ src/app/free-stream/page.js
@@ -5,10 +5,43 @@ export default function Personal() {
   return (
     <>
       <main className={styles.main}>
-        <Link href={"/free-stream"} style={{ textDecoration: "none" }}>
+        <Link
+          href={"/free-stream"}
+          style={{ textDecoration: "none", marginBottom: "1.5rem" }}
+        >
           <h1>Free Stream</h1>
         </Link>
         <IframeWrapper />
+        <p
+          style={{
+            margin: "1rem",
+          }}
+        >
+          Highly suggested to use an Ad blocker like{" "}
+          <Link
+            href={
+              "https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm"
+            }
+            target="_blank"
+          >
+            uBlock
+          </Link>
+          , or {""}
+          <Link
+            href={
+              "https://chromewebstore.google.com/detail/adblock-%E2%80%94-best-ad-blocker/gighmmpiobklfepjocnamgkkbiglidom"
+            }
+            target="_blank"
+          >
+            Adblock.
+          </Link>
+        </p>
+        <Link
+          href={"https://github.com/Sourav9063/watchtogether"}
+          target="_blank"
+        >
+          Please give it a star!⭐
+        </Link>
       </main>
     </>
   );
