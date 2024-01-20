2e51f7acf247146909d09e26ca647fd4f0a83e3f
Sourav <sourav.ahmed5654@gmail.com>
2024-01-21 02:45:23 +0600

    tv check added

--- src/components/iframePlayer/hoverSeEp.js
+++ src/components/iframePlayer/hoverSeEp.js
@@ -1,10 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import styles from "./iframePlayer.module.css";
 import SeasonEpisodeSelector from "./season-episode-selector";
+import { useIframeUrl } from "../Provider/IframeDataProvider";
 
 export default function HoverSeEp() {
-  const [show, setShow] = useState(false);
+  const [iframeUrl] = useIframeUrl();
+  if (!iframeUrl || iframeUrl.type != "tv") return null;
   return (
     <div className={`${styles["hover-se-ep"]} ${styles[""]} `}>
       <div className={`${styles["wrapper"]} ${styles[""]} `}>
