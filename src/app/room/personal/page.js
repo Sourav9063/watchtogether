e57ffcae118c364c3a33e2066fe5c6a9252053ff
Sourav <sourav.ahmed5654@gmail.com>
2024-01-21 02:45:06 +0600

    local hosting

---
+++ src/app/room/personal/page.js
@@ -0,0 +1,16 @@
+import styles from "../../page.module.css";
+import ReactVideoPlayer from "@/components/reactPlayer/ReactPlayer";
+import Link from "next/link";
+import FirebaseVideoPlayerPersonal from "@/components/firebasePlayer/FirebasePlayerPersonal";
+export default function Personal() {
+  return (
+    <>
+      <main className={styles.main}>
+        <Link href={"/"} style={{ textDecoration: "none" }}>
+          <h1>Sync Play</h1>
+        </Link>
+        {false ? <ReactVideoPlayer /> : <FirebaseVideoPlayerPersonal />}
+      </main>
+    </>
+  );
+}
