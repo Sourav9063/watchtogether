3c2e1fca9a0d65aba01e694a06586dbd9e00d618
Sourav <sourav.ahmed5654@gmail.com>
2024-01-21 02:45:14 +0600

    not found added

--- src/components/TmdbSearchResults/TmdbSearchResults.js
+++ src/components/TmdbSearchResults/TmdbSearchResults.js
@@ -1,15 +1,21 @@
 "use client";
 import React from "react";
-import { useSearchResults } from "../Provider/IframeDataProvider";
+import { useQuery, useSearchResults } from "../Provider/IframeDataProvider";
 import TmdbCard from "./TmdbCard";
 import styles from "./TmdbSearchResults.module.css";
 
 export default function TmdbSearchResults() {
   const [searchResults] = useSearchResults();
+  const [query] = useQuery();
+
   if (!searchResults) return null;
   return (
     <div className={`${styles["search-result-wrapper"]} ${styles[""]} `}>
-      <h1>Search Results</h1>
+      {searchResults.length > 0 ? (
+        <h1>Search Results</h1>
+      ) : query.length > 0 ? (
+        <h1>Found Nothing</h1>
+      ) : null}
       <div className={`${styles["cards"]} ${styles[""]} `}>
         {searchResults.map((result) => (
           <TmdbCard key={result?.id} details={result} />
