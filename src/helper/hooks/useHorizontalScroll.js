07e273cdafe74c5b0615c19f7c4a45d2aa725d21
Sourav <ahmed@shellbeehaken.com>
2024-06-27 16:15:59 +0600

    hr scroll issue fixed

--- src/helper/hooks/useHorizontalScroll.js
+++ src/helper/hooks/useHorizontalScroll.js
@@ -1,14 +1,26 @@
 import { useEffect, useRef } from "react";
 
-export function useHorizontalScroll(scrollMultiplier = 3) {
+export function useHorizontalScroll(scrollMultiplier = 1) {
   const elRef = useRef();
   useEffect(() => {
     const el = elRef.current;
     if (el) {
       const onWheel = (e) => {
-        if (e.deltaY == 0) return;
-        e.preventDefault();
-        el.scrollBy({ left: e.deltaY * scrollMultiplier, behavior: "smooth" });
+        if (e.deltaY === 0) return;
+
+        const el = e.currentTarget;
+        if (
+          !(el.scrollLeft === 0 && e.deltaY < 0) &&
+          !(
+            el.scrollWidth - el.clientWidth - Math.round(el.scrollLeft) === 0 &&
+            e.deltaY > 0
+          )
+        ) {
+          e.preventDefault();
+        }
+        el.scrollBy({
+          left: e.deltaY * scrollMultiplier,
+        });
       };
       el.addEventListener("wheel", onWheel);
       return () => el.removeEventListener("wheel", onWheel);
