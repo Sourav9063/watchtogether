4fe79b1063b16bbdece845d79f7c259c5749c766
Sourav <sourav.ahmed5654@gmail.com>
2024-06-30 21:58:14 +0600

    history section created

--- src/components/tmdbSearch/ParseHistory.js
+++ src/components/tmdbSearch/ParseHistory.js
@@ -1,7 +1,5 @@
 import React, { useState, useCallback } from "react";
 import styles from "./tmdbSearch.module.css";
-import { useIframeUrl } from "../Provider/IframeDataProvider";
-import config from "@/config";
 import { useStore } from "@/helper/hooks/useStore";
 import { Constants, Stores } from "@/helper/CONSTANTS";
 import {
@@ -13,6 +11,7 @@ export function PasteHistory() {
   const [historyJson, setHistoryJson] = useState("");
   const [isValidJson, setIsValidJson] = useState(false);
   const [showInput, setShowInput] = useState(false);
+  const [btnTxt, setBtnTxt] = useState("Copy History");
   const autoFocusFn = useCallback(
     (element) => (element ? element.focus() : null),
     []
@@ -20,78 +19,114 @@ export function PasteHistory() {
 
   const [, setSearchResults] = useStore(Stores.searchResults);
   return (
-    <div className={`${styles["search"]} ${styles["paste"]} `}>
-      {!showInput && (
-        <button onClick={() => setShowInput((state) => !state)}>
-          Paste History
-        </button>
-      )}
+    <div className={` ${styles["paste"]} `}>
+      <h1
+        className={`${styles["drop-down"]} `}
+        onClick={() => setShowInput((state) => !state)}
+      >
+        History
+        <span
+          className={`${styles["arrow"]} ${showInput ? styles["open"] : ""} `}
+        ></span>
+      </h1>
       {showInput && (
-        <form
-          className={styles["paste-form"]}
-          action=""
-          onSubmit={(e) => e.preventDefault()}
-        >
-          <input
-            ref={autoFocusFn}
-            autofocus={true}
-            placeholder="Paste JSON"
-            className={styles["paste-input"]}
-            type="text"
-            value={historyJson}
-            onChange={(e) => {
-              const value = e.target.value;
-              setHistoryJson(value);
-              try {
-                JSON.parse(value);
-                setIsValidJson(true);
-              } catch (error) {
-                setIsValidJson(false);
-              }
-            }}
-          />
+        <>
+          {getLocalStorage({
+            key: Constants.LocalStorageKey.WATCH_HISTORY,
+            emptyReturn: null,
+          }) && (
+            <div className={styles["copy-history-section"]}>
+              <button
+                className={`${styles["copy-history"]} ${styles[""]} `}
+                onClick={() => {
+                  const exportJSON = {
+                    [Constants.LocalStorageKey.WATCH_HISTORY]: getLocalStorage({
+                      key: Constants.LocalStorageKey.WATCH_HISTORY,
+                      emptyReturn: [],
+                    }),
+                    [Constants.LocalStorageKey.TV_DATA]: getLocalStorage({
+                      key: Constants.LocalStorageKey.TV_DATA,
+                      emptyReturn: {},
+                    }),
+                  };
+                  navigator.clipboard.writeText(JSON.stringify(exportJSON));
+                  setBtnTxt("Copied");
+                  setTimeout(() => {
+                    setBtnTxt("Copy History");
+                  }, 5000);
+                }}
+              >
+                {btnTxt}
+              </button>
+              <h3>Or</h3>
+            </div>
+          )}
 
-          <button
-            type="submit"
-            onClick={() => {
-              if (!isValidJson) {
-                setShowInput(false);
-                return;
-              }
-              let parsedJson = {};
-              try {
-                parsedJson = JSON.parse(historyJson);
-                addValueLocalStorageArray({
-                  key: Constants.LocalStorageKey.WATCH_HISTORY,
-                  values: parsedJson[Constants.LocalStorageKey.WATCH_HISTORY],
-                });
-                addValueLocalStorageObject({
-                  key: Constants.LocalStorageKey.TV_DATA,
-                  values: parsedJson[Constants.LocalStorageKey.TV_DATA],
-                });
-                setSearchResults((state) => {
-                  if (state.type == "HISTORY") {
-                    return {
-                      ...state,
-                      value: getLocalStorage({
-                        key: Constants.LocalStorageKey.WATCH_HISTORY,
-                        emptyReturn: [],
-                      }),
-                    };
-                  }
-                  return state;
-                });
-                setHistoryJson("");
-                setIsValidJson(false);
-                setShowInput(false);
-              } catch (error) {
-                console.log(error);
-              }
-            }}
+          <form
+            className={styles["paste-form"]}
+            action=""
+            onSubmit={(e) => e.preventDefault()}
           >
-            {isValidJson ? " Save" : "Cancel"}
-          </button>
-        </form>
+            <input
+              ref={autoFocusFn}
+              autofocus={true}
+              placeholder="Paste JSON"
+              className={styles["paste-input"]}
+              type="text"
+              value={historyJson}
+              onChange={(e) => {
+                const value = e.target.value;
+                setHistoryJson(value);
+                try {
+                  JSON.parse(value);
+                  setIsValidJson(true);
+                } catch (error) {
+                  setIsValidJson(false);
+                }
+              }}
+            />
+            <button
+              type="submit"
+              onClick={() => {
+                if (!isValidJson) {
+                  setShowInput(false);
+                  return;
+                }
+                let parsedJson = {};
+                try {
+                  parsedJson = JSON.parse(historyJson);
+                  addValueLocalStorageArray({
+                    key: Constants.LocalStorageKey.WATCH_HISTORY,
+                    values: parsedJson[Constants.LocalStorageKey.WATCH_HISTORY],
+                  });
+                  addValueLocalStorageObject({
+                    key: Constants.LocalStorageKey.TV_DATA,
+                    values: parsedJson[Constants.LocalStorageKey.TV_DATA],
+                  });
+                  setSearchResults((state) => {
+                    if (state.type == "HISTORY") {
+                      return {
+                        ...state,
+                        value: getLocalStorage({
+                          key: Constants.LocalStorageKey.WATCH_HISTORY,
+                          emptyReturn: [],
+                        }),
+                      };
+                    }
+                    return state;
+                  });
+                  setHistoryJson("");
+                  setIsValidJson(false);
+                  setShowInput(false);
+                } catch (error) {
+                  console.log(error);
+                }
+              }}
+            >
+              {isValidJson ? " Save" : "Cancel"}
+            </button>
+          </form>
+        </>
       )}
     </div>
   );
