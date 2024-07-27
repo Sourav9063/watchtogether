b8f161b5e6cce45275e4806e2eb73bd905d83857
Sourav <sourav.ahmed5654@gmail.com>
2024-07-27 19:07:05 +0600

    custom GlobalStore component created

---
+++ src/components/use-store/child.js
@@ -0,0 +1,57 @@
+"use client";
+import { useGlobalStore } from "@/components/Provider/Store";
+import React from "react";
+
+export default function Child() {
+  const [state] = useGlobalStore("GLOBAL");
+  return <h1>GLOBAL:{state.count}</h1>;
+}
+
+export function Child2() {
+  const [state] = useGlobalStore("ANOTHER");
+  return <h1>ANOTHER:{state.another}</h1>;
+}
+export function Child3() {
+  const [state] = useGlobalStore("ANOTHER");
+  const [state2] = useGlobalStore("GLOBAL");
+  return (
+    <>
+      <h1>GLOBAL:{state2.count}</h1>
+      <h1>ANOTHER:{state.another}</h1>
+    </>
+  );
+}
+
+export function Button() {
+  const [state, setState] = useGlobalStore("GLOBAL");
+  return (
+    <button
+      onClick={() => {
+        setState((state) => ({ ...state, count: state.count + 1 }));
+      }}
+    >
+      {state.count}
+    </button>
+  );
+}
+export function Button2() {
+  const [state, setState] = useGlobalStore("ANOTHER");
+  return (
+    <button
+      onClick={() => {
+        setState((state) => ({ ...state, another: state.another + 1 }));
+      }}
+    >
+      {state.another}
+    </button>
+  );
+}
+
+export const H1 = () => {
+  return (
+    <>
+      <h1>Hhi1</h1>
+      <Child />
+    </>
+  );
+};
