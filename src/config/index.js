e0461fb375fa0ae7af41c339351d56766f7a21be
Sourav <ahmed@shellbeehaken.com>
2024-11-20 12:51:54 +0600

    new src

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,8 @@ const config = {
     url5: process.env.NEXT_PUBLIC_PERSONAL_url5,
     url6: process.env.NEXT_PUBLIC_PERSONAL_url6,
     url7: process.env.NEXT_PUBLIC_PERSONAL_url7,
+    url8: process.env.NEXT_PUBLIC_PERSONAL_url8,
+    url9: process.env.NEXT_PUBLIC_PERSONAL_url9,
     urls: [
       process.env.NEXT_PUBLIC_PERSONAL_url1,
       process.env.NEXT_PUBLIC_PERSONAL_url2,
@@ -15,6 +17,8 @@ const config = {
       process.env.NEXT_PUBLIC_PERSONAL_url5,
       process.env.NEXT_PUBLIC_PERSONAL_url6,
       process.env.NEXT_PUBLIC_PERSONAL_url7,
+      process.env.NEXT_PUBLIC_PERSONAL_url8,
+      process.env.NEXT_PUBLIC_PERSONAL_url9,
     ],
   },
   tmdbApiKey: process.env.NEXT_PUBLIC_PERSONAL_tmdb_api_key,
