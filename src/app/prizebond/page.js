5bc2d209aa649e8236ec6fa921585454cdb70461
Sourav Ahmed <sourav.ahmed5654@gmail.com>
2025-09-22 22:56:48 +0600

    export link

--- src/app/prizebond/page.js
+++ src/app/prizebond/page.js
@@ -94,6 +94,10 @@ export default function PrizeBondPage() {
     const now = new Date();
     const dateTime = `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, '0')}-${String(now.getDate()).padStart(2, '0')}_${String(now.getHours()).padStart(2, '0')}-${String(now.getMinutes()).padStart(2, '0')}`;
     XLSX.writeFile(wb, `prizebond-backup-${dateTime}.xlsx`);
+
+    setTimeout(() => {
+      window.open('https://prizebond.ird.gov.bd/MultipleNumbers.php', '_blank');
+    }, 1000);
   };
 
   const handleCheck = (chunkIndex) => {
