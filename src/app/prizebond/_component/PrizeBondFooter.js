6415681d5e22039a35810230d84370ce733dc51a
Sourav Ahmed <sourav.ahmed5654@gmail.com>
2026-07-19 21:08:49 +0600

    feat: text

--- src/app/prizebond/_component/PrizeBondFooter.js
+++ src/app/prizebond/_component/PrizeBondFooter.js
@@ -8,6 +8,16 @@ export default function PrizeBondFooter() {
       <ul>
         {TEXTS.footer.facts.map((fact) => <li key={fact}>{fact}</li>)}
       </ul>
+      <div className={css("pb-footer-meta")}>
+        <p>{TEXTS.footer.copyright}</p>
+        <a
+          href="https://sourav9063.github.io/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {TEXTS.footer.developer}
+        </a>
+      </div>
     </footer>
   );
 }
